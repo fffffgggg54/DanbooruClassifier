9e21e425c29fb350c91f047c512c6e217327bcc9
Fredo Guan <fredo.guan@hotmail.com>
2022-09-05 21:33:38 -0700

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -405,6 +405,7 @@ def trainCycle(image_datasets, model):
                                                     generator=torch.Generator().manual_seed(42)) for x in image_datasets} # set up dataloaders
         lr = FLAGS['learning_rate'] * xm.xrt_world_size()
         device = xm.xla_device()
+        if(FLAGS['num_tpu_cores'] > 1): dataloaders = {x: pl.MPDeviceLoader(dataloaders[x], device) for x in dataloaders}
     
     model = model.to(FLAGS['device'])
 
@@ -452,7 +453,9 @@ def trainCycle(image_datasets, model):
     for epoch in range(FLAGS['num_epochs']):
         epochTime = time.time()
         
-        if (hasTPU == True): xm.master_print("starting epoch: " + str(epoch))
+        if (hasTPU == True):
+            xm.master_print("starting epoch: " + str(epoch))
+            tracker = xm.RateTracker()
         else: print("starting epoch: " + str(epoch))
         AP_regular = []
         AccuracyRunning = []
