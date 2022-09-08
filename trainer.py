8b066ec9a7fdd3526f982555ac162660be49e670
Fredo Guan <fredo.guan@hotmail.com>
2022-09-08 07:28:04 -0700

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -114,7 +114,7 @@ if(FLAGS['device'].type == 'cuda'): FLAGS['use_sclaer'] = True
 
 # dataloader config
 
-FLAGS['batch_size'] = 256
+FLAGS['batch_size'] = 48
 FLAGS['num_workers'] = 4
 if (hasTPU == True): FLAGS['num_workers'] = 22
 if(torch.has_mps == True): FLAGS['num_workers'] = 2
@@ -122,7 +122,7 @@ if(torch.has_mps == True): FLAGS['num_workers'] = 2
 # training config
 
 FLAGS['learning_rate'] = 1e-3
-FLAGS['num_epochs'] = 100
+FLAGS['num_epochs'] = 20
 FLAGS['weight_decay'] = 1e-4
 
 # debugging config
@@ -359,7 +359,7 @@ def getData():
     return image_datasets
 
 def modelSetup(classes):
-    #model = cvt.get_cls_model(len(classes), config=modelConfCust1)
+    model = cvt.get_cls_model(len(classes), config=modelConfCust1)
     #model = cvt.get_cls_model(len(classes), config=modelConf13)
     #model = cvt.get_cls_model(len(classes), config=modelConf21)
     
@@ -368,9 +368,9 @@ def modelSetup(classes):
     #model = models.resnet152()
     #model = models.resnet101(weights=models.ResNet101_Weights.DEFAULT)
     #model = models.resnet34()
-    model = models.resnet34(weights = models.ResNet34_Weights.DEFAULT)
+    #model = models.resnet34(weights = models.ResNet34_Weights.DEFAULT)
     #model = models.resnet18(weights = models.ResNet18_Weights.DEFAULT)
-    model.fc = nn.Linear(model.fc.in_features, len(classes))
+    #model.fc = nn.Linear(model.fc.in_features, len(classes))
     
     #model = TResnetM({'num_classes':len(classes)})
     #model.load_state_dict(torch.load("/home/fredo/Code/ML/danbooru2021/tresnet_m.pth"), strict=False)
