6af4571ef43e0bfd5b9c7db26d18319943c95939
Fredo Guan <fredo.guan@hotmail.com>
2022-09-08 19:19:00 -0700

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -361,16 +361,16 @@ def getData():
 def modelSetup(classes):
     #model = cvt.get_cls_model(len(classes), config=modelConfCust1)
     #model = cvt.get_cls_model(len(classes), config=modelConf13)
-    #model = cvt.get_cls_model(len(classes), config=modelConf21)
+    model = cvt.get_cls_model(len(classes), config=modelConf21)
     
     
-    model = models.resnet152(weights=models.ResNet152_Weights.DEFAULT)
+    #model = models.resnet152(weights=models.ResNet152_Weights.DEFAULT)
     #model = models.resnet152()
     #model = models.resnet101(weights=models.ResNet101_Weights.DEFAULT)
     #model = models.resnet34()
     #model = models.resnet34(weights = models.ResNet34_Weights.DEFAULT)
     #model = models.resnet18(weights = models.ResNet18_Weights.DEFAULT)
-    model.fc = nn.Linear(model.fc.in_features, len(classes))
+    #model.fc = nn.Linear(model.fc.in_features, len(classes))
     
     #model = TResnetM({'num_classes':len(classes)})
     #model.load_state_dict(torch.load("/home/fredo/Code/ML/danbooru2021/tresnet_m.pth"), strict=False)
