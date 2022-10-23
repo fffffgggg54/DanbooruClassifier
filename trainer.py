2175ff559d2dcf1ff6fc7f0893b79c30c68f0186
Fredo Guan <fredo.guan@hotmail.com>
2022-10-23 04:53:14 -0700

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -332,7 +332,7 @@ def trainCycle(image_datasets, model):
                     #loss = criterion(multiAccuracy, referenceTable)
                     #loss = (multiAccuracy - referenceTable).pow(2).sum()
                     #loss = (-torch.log(multiAccuracy[0,4:])).sum()
-                    loss = (1 - multiAccuracy[:,4:]).pow(2).mul((1/MeanStackedAccuracy).to(device2)).sum()
+                    loss = (1 - multiAccuracy[:,4:]).pow(2).mul((1/MeanStackedAccuracyStored).to(device2)).sum()
                     #model.zero_grad()
                     # backward + optimize only if in training phase
                     # TODO this is slow, profile and optimize
