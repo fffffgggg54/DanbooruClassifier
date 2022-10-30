2919e135b14afce0e0e81b9a82a2930f07803054
Fredo Guan <fredo.guan@hotmail.com>
2022-10-29 21:17:22 -0700

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -86,7 +86,7 @@ if(FLAGS['device'] == 'cpu'): FLAGS['num_workers'] = 2
 # training config
 
 FLAGS['num_epochs'] = 50
-FLAGS['learning_rate'] = 1e-2
+FLAGS['learning_rate'] = 3e-4
 FLAGS['weight_decay'] = 1e-2
 FLAGS['gradient_accumulation_iterations'] = 1
 
