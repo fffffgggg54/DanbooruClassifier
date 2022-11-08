a4642525762d81cfb6609e6de3199f4470f374bd
Fredo Guan <fredo.guan@hotmail.com>
2022-11-08 05:47:17 -0800

    levit256 1588 asl balanced training run
    levit-256
    1588 classes
    ASL with balancing using torch.Tensor.mean() to simplify code

--- handleMultiLabel.py
+++ handleMultiLabel.py
@@ -320,7 +320,9 @@ class AsymmetricLossAdaptiveWorking(nn.Module):
             pt = pt0 + pt1
             if(self.adaptive == True):
             
-                gap = pt0.sum() / (y.sum() + self.eps) - pt1.sum() / ((1 - y).sum() + self.eps)
+                #gap = pt0.sum() / (y.sum() + self.eps) - pt1.sum() / ((1 - y).sum() + self.eps)
+                gap = pt0.mean() - pt1.mean()
+                
                 
                 if updateAdaptive == True:
                     self.gamma_neg = self.gamma_neg - self.gamma_step * (gap - self.gap_target)
