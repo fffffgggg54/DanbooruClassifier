47868dbeb5edc3d48c6c6918da4cf30ba1db92dd
Fredo Guan <fredo.guan@hotmail.com>
2022-11-11 16:57:20 -0800

    Update infer.py

--- infer.py
+++ infer.py
@@ -229,7 +229,7 @@ def main():
     #model = cvt.get_cls_model(len(tagNames), config=modelConfCust1)
     #model.load_state_dict(torch.load("models/saved_model_epoch_4.pth", map_location=myDevice))
     model = transformers.AutoModelForImageClassification.from_pretrained("facebook/levit-256", num_labels=len(tagNames), ignore_mismatched_sizes=True)
-    model.load_state_dict(torch.load(rootPath + "models/levit-256-1588-P4-min/saved_model_epoch_3.pth", map_location=myDevice))
+    model.load_state_dict(torch.load(rootPath + "models/levit_256_1588_Hill/saved_model_epoch_14.pth", map_location=myDevice))
     model.eval()   # Set model to evaluate mode
     model = model.to(myDevice)
     
