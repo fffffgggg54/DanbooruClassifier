03b8d0988fd62555cede78624fe276c347f8658b
Fredo Guan <fredo.guan@hotmail.com>
2022-11-22 05:24:46 -0800

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -234,7 +234,7 @@ def modelSetup(classes):
     #model = timm.create_model('ghostnet_050', pretrained=True, num_classes=len(classes))
     model = timm.create_model('convnext_tiny', pretrained=False, num_classes=len(classes))
     
-    model = ml_decoder.add_ml_decoder_head(model)
+    #model = ml_decoder.add_ml_decoder_head(model)
     
     #model = transformers.CvtForImageClassification.from_pretrained('microsoft/cvt-13')
     #model.classifier = nn.Linear(model.config.embed_dim[-1], len(classes))
