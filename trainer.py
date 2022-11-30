ead4bababf912a1b3cb8e23e1445dbb829a2c2a7
Fredo Guan <fredo.guan@hotmail.com>
2022-11-30 15:48:10 -0800

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -58,7 +58,7 @@ FLAGS['tagDFPickle'] = FLAGS['postMetaRoot'] + "tagData.pkl"
 FLAGS['postDFPickleFiltered'] = FLAGS['postMetaRoot'] + "postDataFiltered.pkl"
 FLAGS['tagDFPickleFiltered'] = FLAGS['postMetaRoot'] + "tagDataFiltered.pkl"
 
-FLAGS['modelDir'] = FLAGS['rootPath'] + 'models/levit-384-1588-Hill/'
+FLAGS['modelDir'] = FLAGS['rootPath'] + 'models/regnety_016-1588-Hill/'
 
 
 # post importer config
@@ -94,14 +94,14 @@ if(FLAGS['device'] == 'cpu'): FLAGS['num_workers'] = 2
 
 FLAGS['num_epochs'] = 200
 FLAGS['batch_size'] = 512
-FLAGS['gradient_accumulation_iterations'] = 4
+FLAGS['gradient_accumulation_iterations'] = 1
 
-FLAGS['learning_rate'] = 1e-3
+FLAGS['learning_rate'] = 3e-4
 FLAGS['lr_warmup_epochs'] = 5
 
 FLAGS['weight_decay'] = 2e-2
 
-FLAGS['resume_epoch'] = 22
+FLAGS['resume_epoch'] = 0
 
 # debugging config
 
@@ -281,7 +281,7 @@ def modelSetup(classes):
     
     #model = timm.create_model('efficientnet_b0', pretrained=True, num_classes=len(classes))
     #model = timm.create_model('ghostnet_050', pretrained=True, num_classes=len(classes))
-    #model = timm.create_model('regnetx_002', pretrained=True, num_classes=len(classes))
+    model = timm.create_model('regnety_016', pretrained=True, num_classes=len(classes))
     
     #model = ml_decoder.add_ml_decoder_head(model)
     
@@ -292,7 +292,7 @@ def modelSetup(classes):
 
     # regular huggingface models
 
-    model = transformers.AutoModelForImageClassification.from_pretrained("facebook/levit-384", num_labels=len(classes), ignore_mismatched_sizes=True)
+    #model = transformers.AutoModelForImageClassification.from_pretrained("facebook/levit-384", num_labels=len(classes), ignore_mismatched_sizes=True)
     #model = transformers.AutoModelForImageClassification.from_pretrained("facebook/convnext-tiny-224", num_labels=len(classes), ignore_mismatched_sizes=True)
     
     
@@ -402,7 +402,7 @@ def trainCycle(image_datasets, model):
                 myDataset.transform = transforms.Compose([#transforms.Resize((224,224)),
                                                           #transforms.RandAugment(),
                                                           transforms.TrivialAugmentWide(),
-                                                          danbooruDataset.CutoutPIL(cutout_factor=0.2),
+                                                          #danbooruDataset.CutoutPIL(cutout_factor=0.2),
                                                           transforms.ToTensor(),
                                                           #transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
                                                           ])
@@ -448,8 +448,8 @@ def trainCycle(image_datasets, model):
                     # TODO switch between using autocast and not using it
                     
                     with torch.cuda.amp.autocast():
-                        #outputs = model(imageBatch)
-                        outputs = model(imageBatch).logits
+                        outputs = model(imageBatch)
+                        #outputs = model(imageBatch).logits
                         multiAccuracy = MLCSL.getAccuracy(outputs.to(device2), tagBatch.to(device2))
                         preds = torch.sigmoid(outputs).cpu()
                         outputs = outputs.float()
