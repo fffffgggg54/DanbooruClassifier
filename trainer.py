051260d17bc39f84c59ac5b3b433a0441d78c5e6
Fredo Guan <fredo.guan@hotmail.com>
2022-12-02 22:00:30 -0800

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -104,14 +104,14 @@ FLAGS['lr_warmup_epochs'] = 2
 
 FLAGS['weight_decay'] = 5e-2
 
-FLAGS['resume_epoch'] = 28
+FLAGS['resume_epoch'] = 0
 
 FLAGS['finetune'] = False
 
 # debugging config
 
 FLAGS['verbose_debug'] = False
-FLAGS['skip_test_set'] = False
+FLAGS['skip_test_set'] = True
 FLAGS['stepsPerPrintout'] = 250
 
 classes = None
@@ -362,7 +362,8 @@ def trainCycle(image_datasets, model):
     #ema = MLCSL.ModelEma(model, 0.9997)  # 0.9997^641=0.82
     
     
-    criterion = MLCSL.Hill()
+    #criterion = MLCSL.Hill()
+    criterion = MLCSL.SPLC()
     #criterion = MLCSL.AsymmetricLossOptimized(gamma_neg=5, gamma_pos=5, clip=0.05, eps=1e-8, disable_torch_grad_focal_loss=False)
     #criterion = MLCSL.AsymmetricLossAdaptive(gamma_neg=1, gamma_pos=0, clip=0.05, eps=1e-8, disable_torch_grad_focal_loss=True, adaptive = True, gap_target = 0.1, gamma_step = 0.01)
     #criterion = MLCSL.AsymmetricLossAdaptiveWorking(gamma_neg=1, gamma_pos=0, clip=0.05, eps=1e-8, disable_torch_grad_focal_loss=True, adaptive = True, gap_target = 0.1, gamma_step = 0.2)
