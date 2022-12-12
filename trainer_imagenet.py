3be300ec280d4f8b3eb3a0bbc4d41bac00d91029
Fredo Guan <fredo.guan@hotmail.com>
2022-12-12 00:34:47 -0800

    Update trainer_imagenet.py

--- trainer_imagenet.py
+++ trainer_imagenet.py
@@ -271,7 +271,7 @@ def trainCycle(image_datasets, model):
         
         image_datasets['val'].transform = transforms.Compose([transforms.Resize((256,256)),
             transforms.ToTensor(),
-            transforms.RandomCrop(224)
+            transforms.RandomCrop(224),
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
         ])
         
