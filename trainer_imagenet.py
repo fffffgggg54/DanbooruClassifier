a3bd9892a9539cb25c44da936b0f2fcaaa1c3d1c
Fredo Guan <fredo.guan@hotmail.com>
2022-12-12 01:33:15 -0800

    Update trainer_imagenet.py

--- trainer_imagenet.py
+++ trainer_imagenet.py
@@ -316,7 +316,7 @@ def trainCycle(image_datasets, model):
                             
                             samples += len(images)
                             correct += sum(preds == tagBatch)
-                            tagBatch = torch.eye(len(classes))[tagBatch]
+                            tagBatch = torch.eye(len(classes), device=device)[tagBatch]
                         
                         loss = criterion(outputs.to(device2), tagBatch.to(device2))
 
