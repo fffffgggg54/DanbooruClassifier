5b19a779837713fa51d88b1d7a33b16f472de68a
Fredo Guan <fredo.guan@hotmail.com>
2022-12-14 07:10:12 -0800

    Update handleMultiLabel.py

--- handleMultiLabel.py
+++ handleMultiLabel.py
@@ -205,7 +205,7 @@ class SPLCModified(nn.Module):
         pred = torch.sigmoid(logits)
         
         alpha = 1e-4 if logits.requires_grad else 0
-        self.tau_per_class = self.tau_per_class * (1 - alpha * targets.sum(dim=1)) + alpha * (preds * targets).sum(dim=1)
+        self.tau_per_class = self.tau_per_class * (1 - alpha * targets.sum(dim=0)) + alpha * (preds * targets).sum(dim=0)
         print(self.tau_per_class.mean())
         
         # SPLC missing label correction
