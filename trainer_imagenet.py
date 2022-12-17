dc7cfc545f63b7e6bba6d15bebee256334676175
Fredo Guan <fredo.guan@hotmail.com>
2022-12-17 11:44:45 -0800

    Update trainer_imagenet.py

--- trainer_imagenet.py
+++ trainer_imagenet.py
@@ -273,7 +273,7 @@ def trainCycle(image_datasets, model):
             transforms.TrivialAugmentWide(),
             transforms.ToTensor(),
             RandomErasing(probability=0.5, mode='pixel', device='cpu'),
-            transforms.GaussianBlur()
+            transforms.GaussianBlur(),
             #transforms.ToPILImage(),
             #transforms.ToTensor(),
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
