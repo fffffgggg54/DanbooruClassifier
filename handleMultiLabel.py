013f692658d6c47879cae9494792d29d0a7023ff
Fredo Guan <fredo.guan@hotmail.com>
2022-12-19 20:35:14 -0800

    Update handleMultiLabel.py

--- handleMultiLabel.py
+++ handleMultiLabel.py
@@ -283,6 +283,8 @@ class getDecisionBoundary(nn.Module):
             
             threshold = None
             
+            print(torch.isclose(recall, precision).sum())
+            
             while torch.isclose(recall, precision).sum() > 0:
                 threshold = (threshold_max + threshold_min) / 2
                 predsModified = (preds > threshold).float()
