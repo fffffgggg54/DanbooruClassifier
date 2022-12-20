5f206748fa523af501ea6cc10bbca10a5665b23a
Fredo Guan <fredo.guan@hotmail.com>
2022-12-19 16:28:24 -0800

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -587,6 +587,7 @@ def trainCycle(image_datasets, model):
                     
                    
                     print('[%d/%d][%d/%d]\tLoss: %.4f\tImages/Second: %.4f\tAccuracy: %.2f\tP4: %.2f\t%s' % (epoch, FLAGS['num_epochs'], i, len(dataloaders[phase]), loss, imagesPerSecond, accuracy, multiAccuracy.mean(dim=0) * 100, textOutput))
+                    print(f"[{epoch}/{FLAGS['num_epochs']}][{i}/{len(dataloaders[phase])}]\tLoss: {loss:.4f}\tImages/Second: {imagesPerSecond:.4f}\tAccuracy: {accuracy:.2f}\t {multiAccuracy.mean(dim=0) * 100}\t{textOutput}")
                     #print(id[0])
                     #print(currPostTags)
                     #print(sorted(batchTagAccuracy, key = lambda x: x[1], reverse=True))
