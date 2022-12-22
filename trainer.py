fd5679f483914bd8afd18cc936d65fa0bdf6e1d1
Fredo Guan <fredo.guan@hotmail.com>
2022-12-21 21:27:34 -0800

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -318,7 +318,7 @@ def modelSetup(classes):
     #model = timm.create_model('maxvit_tiny_tf_224.in1k', pretrained=True, num_classes=len(classes))
     #model = timm.create_model('ghostnet_050', pretrained=True, num_classes=len(classes))
     #model = timm.create_model('convnext_base.fb_in22k_ft_in1k', pretrained=True, num_classes=len(classes))
-    model = timm.create_model('gernet_m', pretrained=False, num_classes=len(classes))
+    model = timm.create_model('convnext_tiny', pretrained=False, num_classes=len(classes))
     
     #model = ml_decoder.add_ml_decoder_head(model)
     
