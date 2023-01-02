5beee146c9f4090223825bc18acec55c3e7aa0f5
Fredo Guan <fredo.guan@hotmail.com>
2023-01-02 04:55:26 -0800

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -91,7 +91,7 @@ FLAGS['use_scaler'] = True
 
 # dataloader config
 
-FLAGS['num_workers'] = 33
+FLAGS['num_workers'] = 16
 FLAGS['postDataServerWorkerCount'] = 3
 if(torch.has_mps == True): FLAGS['num_workers'] = 2
 if(FLAGS['device'] == 'cpu'): FLAGS['num_workers'] = 2
