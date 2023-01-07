f73ce97a96e0a2114c74cf8519f0a463435a21c4
Fredo Guan <fredo.guan@hotmail.com>
2023-01-06 18:28:21 -0800

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -100,8 +100,8 @@ if(FLAGS['device'] == 'cpu'): FLAGS['num_workers'] = 2
 # training config
 
 FLAGS['num_epochs'] = 100
-FLAGS['batch_size'] = 256
-FLAGS['gradient_accumulation_iterations'] = 8
+FLAGS['batch_size'] = 64
+FLAGS['gradient_accumulation_iterations'] = 32
 
 FLAGS['base_learning_rate'] = 3e-3
 FLAGS['base_batch_size'] = 2048
