72e4089bd0beb639a679aa09b7810ff4338f4977
Fredo Guan <fredo.guan@hotmail.com>
2023-01-21 22:41:32 -0800

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -207,6 +207,7 @@ elif currGPU == 'm40':
     FLAGS['verbose_debug'] = False
     FLAGS['skip_test_set'] = False
     FLAGS['stepsPerPrintout'] = 50
+    FLAGS['val'] = False
 
 elif currGPU == 'none':
     FLAGS['rootPath'] = "/media/fredo/KIOXIA/Datasets/danbooru2021/"
