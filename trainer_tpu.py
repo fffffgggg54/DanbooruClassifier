dbef33f75518f15a142105f055dc221dbbc5fb8f
Fredo Guan <fredo.guan@hotmail.com>
2023-01-26 07:43:21 -0800

    Update trainer_tpu.py

--- trainer_tpu.py
+++ trainer_tpu.py
@@ -72,7 +72,7 @@ FLAGS['image_size_initial'] = int(FLAGS['imageSize'] // FLAGS['crop'])
 # training config
 
 FLAGS['num_epochs'] = 100
-FLAGS['batch_size'] = 8
+FLAGS['batch_size'] = 64
 FLAGS['gradient_accumulation_iterations'] = 1
 
 FLAGS['base_learning_rate'] = 3e-3
