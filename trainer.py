5a57087715ca2d69a9122be6dacc6fd9019136b5
Fredo Guan <fredo.guan@hotmail.com>
2023-02-05 01:47:08 -0800

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -109,8 +109,8 @@ if currGPU == '3090':
     # training config
 
     FLAGS['num_epochs'] = 100
-    FLAGS['batch_size'] = 128
-    FLAGS['gradient_accumulation_iterations'] = 8
+    FLAGS['batch_size'] = 64
+    FLAGS['gradient_accumulation_iterations'] = 16
 
     FLAGS['base_learning_rate'] = 3e-3
     FLAGS['base_batch_size'] = 2048
