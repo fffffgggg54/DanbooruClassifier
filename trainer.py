fac7077ca8bc545a74d103ed5978c140dd627d1c
Fredo Guan <fredo.guan@hotmail.com>
2023-03-20 06:23:11 -0700

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -216,7 +216,7 @@ if currGPU == '3090':
 elif currGPU == 'm40':
 
 
-    FLAGS['modelDir'] = FLAGS['rootPath'] + 'models/efficientformerv2_s0_448-ASL-BCE-T-5500/'
+    FLAGS['modelDir'] = FLAGS['rootPath'] + 'models/efficientformerv2_s0-ASL-BCE-T-5500/'
 
 
     # post importer config
@@ -228,7 +228,7 @@ elif currGPU == 'm40':
 
     # dataset config
     FLAGS['tagCount'] = 5500
-    FLAGS['image_size'] = 448
+    FLAGS['image_size'] = 224
     FLAGS['progressiveImageSize'] = False
     FLAGS['progressiveSizeStart'] = 0.5
     FLAGS['progressiveAugRatio'] = 3.0
@@ -614,7 +614,7 @@ def modelSetup(classes):
     
     # regular timm models
     
-    model = timm.create_model('efficientformerv2_s0', img_size=448, pretrained=False, num_classes=len(classes), drop_path_rate=0.05)
+    model = timm.create_model('efficientformerv2_s0', pretrained=False, num_classes=len(classes), drop_path_rate=0.05)
     #model = timm.create_model('tf_efficientnetv2_s', pretrained=False, num_classes=len(classes))
     #model = timm.create_model('convnext_base.fb_in22k_ft_in1k', pretrained=True, num_classes=len(classes))
     #model = timm.create_model('gernet_s', pretrained=False, num_classes=len(classes), drop_path_rate = 0.)
