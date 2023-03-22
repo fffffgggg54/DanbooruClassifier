4e4d7a804af4a9f16f968f5d2baaa5a78e8e8df0
Fredo Guan <fredo.guan@hotmail.com>
2023-03-22 14:46:43 -0700

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -324,8 +324,8 @@ elif currGPU == 'v100':
 
     # dataloader config
 
-    FLAGS['num_workers'] = 10
-    FLAGS['postDataServerWorkerCount'] = 2
+    FLAGS['num_workers'] = 20
+    FLAGS['postDataServerWorkerCount'] = 3
     if(torch.has_mps == True): FLAGS['num_workers'] = 2
     if(FLAGS['device'] == 'cpu'): FLAGS['num_workers'] = 2
 
