c45b4979f392832d9a6d2220ca9e0ebcd1e163d7
Fredo Guan <fredo.guan@hotmail.com>
2023-03-21 23:22:56 -0700

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -586,7 +586,7 @@ def getData():
     #trainSet, testSet = torch.utils.data.random_split(trimmedSet, [int(FLAGS['trainSetSize'] * len(trimmedSet)), len(trimmedSet) - int(FLAGS['trainSetSize'] * len(trimmedSet))], generator=torch.Generator().manual_seed(42)) # split dataset
     
     trainSet = getSubsetByID(myDataset, postData, 0, 900)
-    trainSet = getSubsetByID(myDataset, postData, 900, 930)
+    testSet = getSubsetByID(myDataset, postData, 900, 930)
     
     image_datasets = {'train': trainSet, 'val' : testSet}   # put dataset into a list for easy handling
     return image_datasets
