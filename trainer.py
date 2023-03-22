98ea252cd7f54df38dd9b23322d71f3e6d20bffe
Fredo Guan <fredo.guan@hotmail.com>
2023-03-22 03:07:10 -0700

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -700,7 +700,7 @@ def modelSetup(classes):
     #model = timm.create_model('gernet_s', pretrained=False, num_classes=len(classes), drop_path_rate = 0.)
     #model = timm.create_model('edgenext_small', pretrained=False, num_classes=len(classes), drop_path_rate = 0.1)
     #model = timm.create_model('convnext_base', pretrained=False, num_classes=len(classes), drop_path_rate = 0.4)
-    model = timm.create_model('davit_large', pretrained=False, num_classes=len(classes), drop_path_rate = 0.5)
+    model = timm.create_model('davit_large', pretrained=False, num_classes=len(classes), drop_path_rate = 0.5, drop_rate = 0.1)
     #model = timm.create_model('resnet50', pretrained=False, num_classes=len(classes), drop_path_rate = 0.1)
     #model = timm.create_model('efficientnetv2_xl', pretrained=False, num_classes=len(classes), drop_path_rate = 0.6)
     #model = timm.create_model('davit_tiny', pretrained=False, num_classes=len(classes), drop_path_rate = 0.1)
