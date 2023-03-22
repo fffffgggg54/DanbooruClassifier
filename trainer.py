6b4266ee7431751c122feb43bf090ee2943ab2b6
Fredo Guan <fredo.guan@hotmail.com>
2023-03-21 18:53:51 -0700

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -291,7 +291,7 @@ elif currGPU == 'v100':
 
 
 
-    FLAGS['modelDir'] = FLAGS['rootPath'] + 'models/vit_base_patch8_224-448-ASL_BCE_T-1588_v100/'
+    FLAGS['modelDir'] = FLAGS['rootPath'] + 'models/vit_base_patch16_224-448-ASL_BCE_T-1588_v100/'
 
 
     # post importer config
@@ -689,7 +689,7 @@ def modelSetup(classes):
     
     #model = timm.create_model('efficientformerv2_s0', pretrained=False, num_classes=len(classes), drop_path_rate=0.05)
     #model = timm.create_model('tf_efficientnetv2_s', pretrained=False, num_classes=len(classes))
-    model = timm.create_model('vit_base_patch8_224', img_size=448, pretrained=False, num_classes=len(classes), drop_path_rate=0.4)
+    model = timm.create_model('vit_base_patch16_224', img_size=448, pretrained=False, num_classes=len(classes), drop_path_rate=0.4)
     #model = timm.create_model('gernet_s', pretrained=False, num_classes=len(classes), drop_path_rate = 0.)
     #model = timm.create_model('edgenext_small', pretrained=False, num_classes=len(classes), drop_path_rate = 0.1)
     #model = timm.create_model('davit_base', pretrained=False, num_classes=len(classes), drop_path_rate = 0.4)
