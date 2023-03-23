e7f8e23c01e32b58cd75bb71b1905cecf7d77589
Fredo Guan <fredo.guan@hotmail.com>
2023-03-23 07:17:23 -0700

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -968,7 +968,7 @@ def trainCycle(image_datasets, model):
                         boundary = boundaryCalculator(preds, tagBatch)
                         if FLAGS['use_ddp'] == True:
                             torch.distributed.all_reduce(boundaryCalculator.thresholdPerClass, op = torch.distributed.ReduceOp.AVG)
-                            boundary = boundaryCalculator.thresholdPerClass.detatch()
+                            boundary = boundaryCalculator.thresholdPerClass.detach()
 
                         #predsModified=preds
                         #multiAccuracy = MLCSL.getAccuracy(predsModified.to(device2), tagBatch.to(device2))
