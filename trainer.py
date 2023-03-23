7e75d62daf79f3ea38fd61784b37ddf48a8285da
Fredo Guan <fredo.guan@hotmail.com>
2023-03-23 03:39:50 -0700

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -345,8 +345,8 @@ elif currGPU == 'v100':
     FLAGS['resume_epoch'] = 0
 
     FLAGS['finetune'] = False
-    FLAGS['compile_model'] = True
-    FLAGS['fast_norm'] = not FLAGS['compile_model']
+    FLAGS['compile_model'] = False
+    FLAGS['fast_norm'] = False
     FLAGS['channels_last'] = FLAGS['use_AMP']
 
     # debugging config
