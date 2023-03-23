91f73b85f5827dc963dbd7a31a934af4e0e09fab
Fredo Guan <fredo.guan@hotmail.com>
2023-03-23 10:45:15 -0700

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -294,7 +294,7 @@ elif currGPU == 'v100':
     #FLAGS['modelDir'] = FLAGS['rootPath'] + 'models/gc_efficientnetv2_rw_t-448-ASL_BCE_T-1588/'
     #FLAGS['modelDir'] = FLAGS['rootPath'] + 'models/convnext_tiny-448-ASL_BCE-1588/'
     #FLAGS['modelDir'] = FLAGS['rootPath'] + 'models/convnext_tiny-448-ASL_BCE_T-1588/'
-    FLAGS['modelDir'] = FLAGS['rootPath'] + 'models/tresnet_m-224-ASL_BCE_T-1588/'
+    FLAGS['modelDir'] = FLAGS['rootPath'] + 'models/rexnet_130-224-ASL_BCE_T-1588/'
 
     # post importer config
 
@@ -349,8 +349,8 @@ elif currGPU == 'v100':
     FLAGS['threshold_loss'] = True
 
     FLAGS['finetune'] = False
-    FLAGS['compile_model'] = False
-    FLAGS['fast_norm'] = True
+    FLAGS['compile_model'] = True
+    FLAGS['fast_norm'] = False
     FLAGS['channels_last'] = FLAGS['use_AMP']
 
     # debugging config
@@ -709,7 +709,7 @@ def modelSetup(classes):
     #model = timm.create_model('efficientnetv2_xl', pretrained=False, num_classes=len(classes), drop_path_rate = 0.6)
     #model = timm.create_model('davit_tiny', pretrained=False, num_classes=len(classes), drop_path_rate = 0.1)
     #model = timm.create_model('convnext_tiny', pretrained=False, num_classes=len(classes), drop_path_rate = 0.15, drop_rate=0.05)
-    model = timm.create_model('tresnet_m', pretrained=False, num_classes=len(classes), drop_rate=0.05)
+    model = timm.create_model('rexnet_130', pretrained=False, num_classes=len(classes), drop_path_rate = 0.1, drop_rate=0.05)
     
     # gap model
     '''
