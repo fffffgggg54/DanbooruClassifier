90e6a346e8abbd5fdbeba4bb1d6d6cd0be03fc57
Fredo Guan <fredo.guan@hotmail.com>
2023-03-24 01:28:42 -0700

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -335,8 +335,8 @@ elif currGPU == 'v100':
     # training config
 
     FLAGS['num_epochs'] = 100
-    FLAGS['batch_size'] = 128
-    FLAGS['gradient_accumulation_iterations'] = 8
+    FLAGS['batch_size'] = 256
+    FLAGS['gradient_accumulation_iterations'] = 4
 
     FLAGS['base_learning_rate'] = 3e-3
     FLAGS['base_batch_size'] = 2048
@@ -710,7 +710,7 @@ def modelSetup(classes):
     #model = timm.create_model('resnet50', pretrained=False, num_classes=len(classes), drop_path_rate = 0.1)
     #model = timm.create_model('efficientnetv2_xl', pretrained=False, num_classes=len(classes), drop_path_rate = 0.6)
     #model = timm.create_model('davit_tiny', pretrained=False, num_classes=len(classes), drop_path_rate = 0.1)
-    model = timm.create_model('efficientnetv2_rw_t', pretrained=False, num_classes=len(classes), drop_path_rate = 0.15, drop_rate=0.02)
+    model = timm.create_model('convnext_nano', pretrained=False, num_classes=len(classes), drop_path_rate = 0.15, drop_rate=0.02)
     #model = timm.create_model('regnetz_b16', pretrained=False, num_classes=len(classes), drop_path_rate = 0.1, drop_rate=0.05)
     
     # gap model
