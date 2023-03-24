bce28a8fbc32d5002611001be66c7282b95733e8
Fredo Guan <fredo.guan@hotmail.com>
2023-03-24 00:44:39 -0700

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -335,7 +335,7 @@ elif currGPU == 'v100':
     # training config
 
     FLAGS['num_epochs'] = 100
-    FLAGS['batch_size'] = 128
+    FLAGS['batch_size'] = 256
     FLAGS['gradient_accumulation_iterations'] = 4
 
     FLAGS['base_learning_rate'] = 3e-3
@@ -343,7 +343,7 @@ elif currGPU == 'v100':
     FLAGS['learning_rate'] = ((FLAGS['batch_size'] * FLAGS['gradient_accumulation_iterations']) / FLAGS['base_batch_size']) * FLAGS['base_learning_rate']
     FLAGS['lr_warmup_epochs'] = 5
 
-    FLAGS['weight_decay'] = 1e-4
+    FLAGS['weight_decay'] = 2e-2
 
     FLAGS['resume_epoch'] = 0
     
@@ -351,8 +351,8 @@ elif currGPU == 'v100':
     FLAGS['splc'] = False
 
     FLAGS['finetune'] = False
-    FLAGS['compile_model'] = False
-    FLAGS['fast_norm'] = True
+    FLAGS['compile_model'] = True
+    FLAGS['fast_norm'] = False
     FLAGS['channels_last'] = FLAGS['use_AMP']
 
     # debugging config
@@ -710,8 +710,8 @@ def modelSetup(classes):
     #model = timm.create_model('resnet50', pretrained=False, num_classes=len(classes), drop_path_rate = 0.1)
     #model = timm.create_model('efficientnetv2_xl', pretrained=False, num_classes=len(classes), drop_path_rate = 0.6)
     #model = timm.create_model('davit_tiny', pretrained=False, num_classes=len(classes), drop_path_rate = 0.1)
-    #model = timm.create_model('convnext_tiny', pretrained=False, num_classes=len(classes), drop_path_rate = 0.15, drop_rate=0.05)
-    model = timm.create_model('regnetz_b16', pretrained=False, num_classes=len(classes), drop_path_rate = 0.1, drop_rate=0.05)
+    model = timm.create_model('convnext_pico', pretrained=False, num_classes=len(classes), drop_path_rate = 0.1, drop_rate=0.)
+    #model = timm.create_model('regnetz_b16', pretrained=False, num_classes=len(classes), drop_path_rate = 0.1, drop_rate=0.05)
     
     # gap model
     '''
