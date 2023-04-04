1a77bd90002a1127ef6a02e3eb3d1c775636c71f
Fredo Guan <fredo.guan@hotmail.com>
2023-04-04 00:13:01 -0700

    Update infer.py

--- infer.py
+++ infer.py
@@ -187,7 +187,7 @@ def main():
         
 
         transform = transforms.Compose([
-            transforms.Resize((288,288)),
+            transforms.Resize((320,320)),
             transforms.ToTensor(),
             #transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
             ])
