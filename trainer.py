d7b6d259365524eae20606bc4894213fa460b476
Fredo Guan <fredo.guan@hotmail.com>
2023-04-04 20:59:07 -0700

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -337,7 +337,7 @@ elif currGPU == 'v100':
 
     FLAGS['num_epochs'] = 100
     FLAGS['batch_size'] = 96
-    FLAGS['gradient_accumulation_iterations'] = 20
+    FLAGS['gradient_accumulation_iterations'] = 4
 
     FLAGS['base_learning_rate'] = 3e-3
     FLAGS['base_batch_size'] = 2048
