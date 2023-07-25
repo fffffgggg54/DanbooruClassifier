979b4a3d3c48ad3037ffe4f1dff2b12202bf568b
Fredo Guan <fredo.guan@hotmail.com>
2023-07-25 10:51:49 -0700

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -136,7 +136,7 @@ if currGPU == 'v100':
     FLAGS['batch_size'] = 96
     FLAGS['fast_norm'] = True
     FLAGS['channels_last'] = True
-    FLAGS['compile'] = False
+    FLAGS['compile_model'] = False
     
 
 
