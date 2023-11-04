cccab56ffd93778cb123400bf66f828522b3f688
Fredo Guan <fredo.guan@hotmail.com>
2023-11-04 05:52:52 -0700

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -360,7 +360,7 @@ elif currGPU == 'v100':
 
     FLAGS['resume_epoch'] = 0
     
-    FLAGS['threshold_loss'] = False
+    FLAGS['threshold_loss'] = True
     FLAGS['threshold_multiplier'] = 1.0
     FLAGS['splc'] = False
     FLAGS['splc_start_epoch'] = 1
