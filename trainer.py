36f73f5ad8aece469e83bd771e0ef30f44fd88e0
Fredo Guan <fredo.guan@hotmail.com>
2023-11-23 05:02:05 -0700

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -1128,7 +1128,7 @@ def trainCycle(image_datasets, model):
                         #loss = (1 - multiAccuracy[:,4:]).div(MeanStackedAccuracyStored.to(device2)).pow(2).sum()
                         #loss = (1 - multiAccuracy[:,8]).pow(2).sum()
                         #loss = -MLCSL.getSingleMetric(outputs.sigmoid(), tagsModified, MLCSL.PU_F_Metric).sum()
-                        loss = -MLCSL.AUL(outputs.sigmoid(), tagsModified)
+                        loss = -MLCSL.AUL(outputs.sigmoid(), tagsModified).sum()
                         #model.zero_grad()
                         
                         
