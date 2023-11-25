4a2d1c5cfa06c353da1967861be88f84e2f23897
Fredo Guan <fredo.guan@hotmail.com>
2023-11-25 03:09:50 -0700

    Update handleMultiLabel.py

--- handleMultiLabel.py
+++ handleMultiLabel.py
@@ -1344,7 +1344,7 @@ def chart_inner(preds):
     # [K, B, B]
     #result = (sample1 == sample2).int() * 0.5 + (sample1 > sample2).int()
     result = (sample1 - sample2).sigmoid()
-    print(result)
+    #print(result)
     return result
     
 def AUROC(preds, targs, epsilon):
@@ -1361,7 +1361,7 @@ def AUL(preds, targs, epsilon = 1e-8):
     numel = targs.size(dim=0) # = K
     # [K]
     result = (num_pos > 0).int() * chart_inner(preds).sum(dim=(1,2)) / (num_pos * numel + epsilon)
-    #print(result)
+    print(result)
     return result
 
 # tracking for performance metrics that can be computed from confusion matrix
