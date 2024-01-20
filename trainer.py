cd47c134f4fb81c815f843cd91a662d2342e94f2
Fredo Guan <fredo.guan@hotmail.com>
2024-01-20 08:23:28 -0700

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -353,8 +353,8 @@ elif currGPU == 'v100':
     # training config
 
     FLAGS['num_epochs'] = 50
-    FLAGS['batch_size'] = 96
-    FLAGS['gradient_accumulation_iterations'] = 4
+    FLAGS['batch_size'] = 1
+    FLAGS['gradient_accumulation_iterations'] = 1
 
     FLAGS['base_learning_rate'] = 3e-3
     FLAGS['base_batch_size'] = 2048
@@ -1308,6 +1308,7 @@ def trainCycle(image_datasets, model):
                                 with open(FLAGS['modelDir'] + "mem_snapshot.pickle", "wb") as f:
                                     dump(s, f)
                                 torch.cuda.memory._record_memory_history(enabled=None)
+                            exit()
                         
                         if (phase == 'val'):
                             # for mAP calculation
