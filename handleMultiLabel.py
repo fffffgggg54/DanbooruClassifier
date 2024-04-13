gradient leak fix attempt

handleMultiLabel.py
@@ -1412,26 +1412,27 @@ class MetricTracker():
         return self.get_full_metrics().mean(dim=0)
     
     def update(self, preds, targs):
-        self.sampleCount += targs.size(dim=0)
-        
-        targs_inv = 1 - targs
-        P = targs * preds
-        N = targs_inv * preds
-        
-        
-        TP = P.sum(dim=0)
-        FN = (targs - P).sum(dim=0)
-        FP = N.sum(dim=0)
-        TN = (targs_inv - N).sum(dim=0)
-        
-        output = torch.stack([TP, FN, FP, TN])
-        if self.running_confusion_matrix is None:
-            self.running_confusion_matrix = output
-        
-        else:
-            self.running_confusion_matrix += output
+        with torch.no_grad():
+            self.sampleCount += targs.size(dim=0)
             
-        return self.get_aggregate_metrics()
+            targs_inv = 1 - targs
+            P = targs * preds
+            N = targs_inv * preds
+            
+            
+            TP = P.sum(dim=0)
+            FN = (targs - P).sum(dim=0)
+            FP = N.sum(dim=0)
+            TN = (targs_inv - N).sum(dim=0)
+            
+            output = torch.stack([TP, FN, FP, TN])
+            if self.running_confusion_matrix is None:
+                self.running_confusion_matrix = output
+            
+            else:
+                self.running_confusion_matrix += output
+                
+            return self.get_aggregate_metrics()
         
 
 class AverageMeter(object):

trainer.py
@@ -1192,7 +1192,7 @@ def trainCycle(image_datasets, model):
                         
                             # update boundary
                             boundaryCalculator(
-                                preds,
+                                preds.detach(),
                                 tagBatch,
                                 update=(phase == "train"),
                                 step_opt=((i+1) % FLAGS['gradient_accumulation_iterations'] == 0)
@@ -1211,7 +1211,7 @@ def trainCycle(image_datasets, model):
                         #multiAccuracy = MLCSL.getAccuracy(predsModified.to(device2), tagBatch.to(device2))
                         with torch.no_grad():
                             #multiAccuracy = cm_tracker.update((preds.detach() > boundary.detach()).float().to(device), tagBatch.to(device))
-                            multiAccuracy = cm_tracker.update(preds.detach().float().to(device), tagBatch.to(device))
+                            multiAccuracy = cm_tracker.update(preds.detach(), tagBatch.to(device))
                         
                         
                         
@@ -1447,6 +1447,7 @@ def trainCycle(image_datasets, model):
                 
             currPhase += 1
             optimizer.zero_grad(set_to_none=True)
+            if(boundaryCalculator.opt): boundaryCalculator.zero_grad(set_to_none=True)
             #mlr_act_opt.zero_grad(set_to_none=True)
 
             '''