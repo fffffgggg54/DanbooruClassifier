057ee815b68b393605df90deea00f725f337459f
Fredo Guan <fredo.guan@hotmail.com>
2024-08-08 04:10:28 -0700

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -1257,7 +1257,7 @@ def trainCycle(image_datasets, model):
                                 all_logits = torch.zeros(dist.get_world_size() * outputs.shape[0], outputs.shape[1], device=outputs.device)
                                 torch.distributed.all_gather_into_tensor(all_logits.to(outputs.dtype), outputs)
                                 all_tags = torch.zeros_like(all_logits)
-                                torch.distributed.all_gather_into_tensor(all_tags, tagbatch)
+                                torch.distributed.all_gather_into_tensor(all_tags, tagBatch)
 
                                 dist_tracker(all_logits.to(torch.float64), all_tags)
 
