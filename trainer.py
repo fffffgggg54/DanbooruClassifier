76202ab1e27a84e2bb8d8ca1f38db7f11205206c
Fredo Guan <fredo.guan@hotmail.com>
2024-08-07 21:45:21 -0700

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -1255,7 +1255,7 @@ def trainCycle(image_datasets, model):
                                 multiAccuracy = cm_tracker.update(preds.detach(), tagBatch.to(device))
                                 dist_tracker(outputs.detach(), tagBatch.to(device))
                                 tracker_dumps = [None for _ in range(dist.get_world_size())]
-                                torch.distributed.all_gather_object(tracker_dumps, dist_tracker.dump())
+                                torch.distributed.all_gather(tracker_dumps, dist_tracker.dump())
                                 tracker_dumps.pop(dist.get_rank())
                                 for curr_dump in tracker_dumps:
                                     dist_tracker.update(curr_dump)
