bad9eaf8008cf2b231dfd3457a18ad5fb26dd0a7
Fredo Guan <fredo.guan@hotmail.com>
2024-08-08 04:19:23 -0700

    Update handleMultiLabel.py

--- handleMultiLabel.py
+++ handleMultiLabel.py
@@ -625,8 +625,9 @@ class DistributionTracker(nn.Module):
     def neg_std(self): return self.neg_var ** 0.5
     
     def dump(self):
-        #return torch.stack([self._pos_mean, self._pos_count, self._pos_M2, self._neg_mean, self._neg_count, self._neg_M2])
-        return torch.stack([self._pos_mean, self._pos_count, self._pos_var, self._neg_mean, self._neg_count, self._neg_var])
+        return torch.stack([self._pos_mean, self._pos_count, self._pos_M2, self._neg_mean, self._neg_count, self._neg_M2])
+    
+    #return torch.stack([self._pos_mean, self._pos_count, self._pos_var, self._neg_mean, self._neg_count, self._neg_var])
     '''
     def update(self, dump):
         deltaPos = dump[0] - self._pos_mean
