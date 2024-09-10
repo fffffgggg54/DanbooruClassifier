543aea4fc6f2f747f1cee46261dfc5a8a67c9569
Fredo Guan <fredo.guan@hotmail.com>
2024-09-10 14:32:56 -0700

    asdf

--- handleMultiLabel.py
+++ handleMultiLabel.py
@@ -617,8 +617,8 @@ def generate_loss_weights(logits, labels, dist_tracker, clip_dist=0.95, eps=1e-8
     logit_p_values = z_score_to_p_value(logit_z_scores)
     
     # [B, K]
-    #loss_weights = torch.exp(-logit_z_scores)
-    loss_weights = torch.ones_like(labels)
+    loss_weights = torch.exp(-logit_z_scores)
+    #loss_weights = torch.ones_like(labels)
     #loss_weights = loss_weights.where(class_p_values > clip_dist, 1).where(labels == 1, 1)
     loss_weights *= (dist_tracker.neg_count / (dist_tracker.pos_count + eps)).where(labels == 1, 1)
     return loss_weights
