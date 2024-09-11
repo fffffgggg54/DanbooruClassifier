935fc0e439ac3d94856b3c766a5f3ea430261ce7
Fredo Guan <fredo.guan@hotmail.com>
2024-09-11 08:38:29 -0700

    Update handleMultiLabel.py

--- handleMultiLabel.py
+++ handleMultiLabel.py
@@ -667,7 +667,7 @@ class DistributionTracker(nn.Module):
     
         return torch.stack([self._pos_mean, self._pos_count, self._pos_var, self._neg_mean, self._neg_count, self._neg_var])
     
-    def set_device（self, device):
+    def set_device(self, device):
         self._pos_mean.to(device)
         self._pos_count.to(device)
         self._pos_var.to(device)
