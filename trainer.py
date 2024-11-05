eeadce2d2c7ef25a580b2e011ac7d4c781628b09
Fredo Guan <fredo.guan@hotmail.com>
2024-11-05 00:17:30 -0700

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -1028,7 +1028,7 @@ def modelSetup(classes):
     return model
     
 def getDataLoader(dataset, batch_size, epoch):
-    distSampler = DistributedSampler(dataset=dataset, seed=17, drop_last=True)
+    distSampler = DistributedSampler(dataset=dataset, shuffle=True, seed=17, drop_last=True)
     distSampler.set_epoch(epoch)
     return torch.utils.data.DataLoader(dataset, batch_size = batch_size, sampler=distSampler, num_workers=FLAGS['num_workers'], persistent_workers = True, prefetch_factor=2, pin_memory = True, generator=torch.Generator().manual_seed(41))
 
