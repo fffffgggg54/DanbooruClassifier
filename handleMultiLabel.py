f1f19a34f84fc4ba88b86b016c7dc2d295408db8
Fredo Guan <fredo.guan@hotmail.com>
2024-12-08 15:38:40 -0700

    update

--- handleMultiLabel.py
+++ handleMultiLabel.py
@@ -373,10 +373,11 @@ class DualLogisticRegression_Head(nn.Module):
             
             '''
             with torch.no_grad():
-                propensity = 1 / (1 + torch.exp(-self.estimator(x.detach())) + self.eps)
+                #propensity = 1 / (1 + torch.exp(-self.estimator(x.detach())) + self.eps)
+                propensity = 1 / (1 + self.estimator(x.detach()) ** 2 + self.eps)
             
-            #return propensity / (1 + self.estimator(x.detach()) ** 2 + torch.exp(-self.fc(x)) + self.eps)
-            return propensity / (1 + torch.exp(-self.estimator(x.detach())) + torch.exp(-self.fc(x)) + self.eps)
+            return propensity / (1 + self.estimator(x.detach()) ** 2 + torch.exp(-self.fc(x)) + self.eps)
+            #return propensity / (1 + torch.exp(-self.estimator(x.detach())) + torch.exp(-self.fc(x)) + self.eps)
 
 
 class DualLogisticRegression(nn.Module):
