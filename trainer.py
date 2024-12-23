e7cd58442e33d5643cd321be83c8041cb022f3bc
Fredo Guan <fredo.guan@hotmail.com>
2024-12-23 03:57:20 -0800

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -1413,6 +1413,10 @@ def trainCycle(image_datasets, model):
                             if (FLAGS['use_scaler'] == True):   # cuda gpu case
                                 with model.no_sync():
                                     scaler.scale(loss).backward()
+                                    for p in optimizer.param_groups[0]['params']:
+                                        print(p.grad)
+                                    for p in model.parameters():
+                                        print(p.grad)
                                 if((i+1) % FLAGS['gradient_accumulation_iterations'] == 0):
                                     torch.cuda.synchronize()
                                     nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0, norm_type=2)
