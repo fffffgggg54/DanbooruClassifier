178d22c5a045e1ca85a9db4add9e8eb67fdef959
Fredo Guan <fredo.guan@hotmail.com>
2025-06-30 03:18:24 -0700

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -467,15 +467,15 @@ elif currGPU == 'sol_gh200':
 
     # dataloader config
 
-    FLAGS['num_workers'] = 35
-    FLAGS['postDataServerWorkerCount'] = 10
+    FLAGS['num_workers'] = 10
+    FLAGS['postDataServerWorkerCount'] = 3
     if(FLAGS['device'] == 'cpu'): FLAGS['num_workers'] = 2
 
     # training config
 
     FLAGS['num_epochs'] = 50
-    FLAGS['batch_size'] = 1024
-    FLAGS['gradient_accumulation_iterations'] = 3
+    FLAGS['batch_size'] = 256
+    FLAGS['gradient_accumulation_iterations'] = 12
 
     FLAGS['base_learning_rate'] = 3e-3
     FLAGS['base_batch_size'] = 2048
