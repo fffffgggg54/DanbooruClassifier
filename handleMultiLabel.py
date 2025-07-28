0ffba818936f0489954fe128ebebb7b68ce3b965
Fredo Guan <fredo.guan@hotmail.com>
2025-07-28 14:20:42 -0700

    Update handleMultiLabel.py

--- handleMultiLabel.py
+++ handleMultiLabel.py
@@ -513,6 +513,7 @@ class ClassEmbedClassifierHead(nn.Module):
         q = q.expand(x.shape[0], -1, -1) # [B, K, D]
         x = torch.cat([x.unsqueeze(1).expand(-1, q.shape[1], -1), q], dim=-1) # [B, K, C+D]
         x = self.ffn(x).flatten(-1) # [B, K]
+        print(x.shape)
 
         return x
 
