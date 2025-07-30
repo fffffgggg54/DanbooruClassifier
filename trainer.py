e15dc7367971457f24e7854e491f08e8e3d42bac
Fredo Guan <fredo.guan@hotmail.com>
2025-07-30 16:51:30 -0700

    1588 davit run

--- trainer.py
+++ trainer.py
@@ -435,9 +435,9 @@ elif currGPU == 'v100':
     FLAGS['val'] = False
 
 elif currGPU == 'sol_gh200':
-    FLAGS['modelDir'] = "/scratch/fyguan/danbooru_models/scratch"
+    #FLAGS['modelDir'] = "/scratch/fyguan/danbooru_models/scratch"
     #FLAGS['modelDir'] = "/scratch/fyguan/danbooru_models/davit_tiny-ml_decoder_NoPostInProjAct_no_dupe_OnlyClassEmbed_gte_L_en_v1_5dNoNorm1024_sharedFC-ASL_BCE_T-dist_log_odds-224-1588-50epoch/"
-    #FLAGS['modelDir'] = "/scratch/fyguan/danbooru_models/convformer_s18-ml_decoder_NoMlp_no_dupe_OnlyClassEmbed_gte_L_en_v1_5dNoNorm1024_sharedFC-ASL_BCE_T-dist_log_odds-224-5500-50epoch/"
+    FLAGS['modelDir'] = "/scratch/fyguan/danbooru_models/davit_tiny-ml_decoder_NoMlp_no_dupe_OnlyClassEmbed_gte_L_en_v1_5dNoNorm1024_sharedFC-ASL_BCE_T-dist_log_odds-224-1588-50epoch/"
     #FLAGS['modelDir'] = "/scratch/fyguan/danbooru_models/convformer_s18-ml_decoder_NoMlp_no_dupe_OnlyClassEmbed_gte_L_en_v1_5dNoNorm1024_sharedFC-ASL_BCE_T-dist_log_odds-224-1588-50epoch/"
     # post importer config
 
@@ -492,7 +492,7 @@ elif currGPU == 'sol_gh200':
     
     FLAGS['use_mlr_act'] = False
     FLAGS['use_matryoshka_head'] = False
-    FLAGS['use_class_embed_head'] = True
+    FLAGS['use_class_embed_head'] = False
 
     FLAGS['logit_offset'] = True
     FLAGS['logit_offset_multiplier'] = 1.0
@@ -1180,7 +1180,7 @@ def modelSetup(classes):
         use_mlp = False,
     )
     '''
-    '''
+    
     model = ml_decoder.add_ml_decoder_head(
         model,
         num_groups = 0,
@@ -1192,7 +1192,7 @@ def modelSetup(classes):
         attn_out_proj = True,
         use_mlp = False,
     )
-    '''
+    
     num_features = model.num_features
 
     if FLAGS['use_mlr_act'] == True or FLAGS['use_matryoshka_head'] == True or FLAGS['use_class_embed_head'] == True:
