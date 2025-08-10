99ad16b56d30445a5c7a12458269c7230c484a2f
Fredo Guan <fredo.guan@hotmail.com>
2025-08-10 04:49:28 -0700

    Update handleMultiLabel.py

--- handleMultiLabel.py
+++ handleMultiLabel.py
@@ -506,7 +506,7 @@ class CrossSwiGLU(nn.Module):
 
     def forward(self, x, q):
         x = self.fc1_x(x)
-        gate = self.fc1_q(x)
+        gate = self.fc1_q(q)
         x = self.act(gate) * x
         x = self.drop1(x)
         x = self.norm(x)
