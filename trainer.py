8c6d1f7b159fa6401b72d6be3d10ae4416faefbe
Fredo Guan <fredo.guan@hotmail.com>
2025-08-19 20:01:02 -0700

    increase BS

--- trainer.py
+++ trainer.py
@@ -576,8 +576,8 @@ elif currGPU == 'sol_multi':
     # training config
 
     FLAGS['num_epochs'] = 50
-    FLAGS['batch_size'] = 256
-    FLAGS['gradient_accumulation_iterations'] = 6
+    FLAGS['batch_size'] = 384
+    FLAGS['gradient_accumulation_iterations'] = 4
 
     FLAGS['base_learning_rate'] = 3e-3
     FLAGS['base_batch_size'] = 2048
