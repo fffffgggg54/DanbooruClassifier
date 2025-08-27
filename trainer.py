877251592a09687b1ba48a0ec0eb6b343b737972
Fredo Guan <fredo.guan@hotmail.com>
2025-08-27 12:08:46 -0700

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -538,7 +538,8 @@ elif currGPU == 'sol_gh200':
 elif currGPU == 'sol_multi':
     #FLAGS['modelDir'] = "/scratch/fyguan/danbooru_models/scratch/"
     #FLAGS['modelDir'] = "/scratch/fyguan/danbooru_models/davit_tiny-OV_1_of_5_seed42-classEmbedGatingHead2048_gte_L_en_v1_5dNoNorm1024-ASL_BCE_T-dist_log_odds_W-InvClassProp-224-1588-50epoch/"
-    FLAGS['modelDir'] = "/scratch/fyguan/danbooru_models/davit_tiny-OV_1_of_5_seed42-classEmbedGatingHead2048_HighDrop_PreNorm_QueryNoiseAug_RandQueryAug_gte_L_en_v1_5dNoNorm1024-ASL_BCE_T-dist_log_odds_W-InvClassProp-224-1588-50epoch/"
+    #FLAGS['modelDir'] = "/scratch/fyguan/danbooru_models/davit_tiny-OV_1_of_5_seed42-classEmbedGatingHead2048_HighDrop_PreNorm_QueryNoiseAug_RandQueryAug_gte_L_en_v1_5dNoNorm1024-ASL_BCE_T-dist_log_odds_W-InvClassProp-224-1588-50epoch/"
+    FLAGS['modelDir'] = "/scratch/fyguan/danbooru_models/davit_tiny-OV_1_of_5_seed42-classEmbedGatingHead2048_QueryNoiseAug_PosNegRandQueryAug_gte_L_en_v1_5dNoNorm1024-ASL_BCE_T-dist_log_odds-224-1588-50epoch/"
 
     # post importer config
 
@@ -578,8 +579,8 @@ elif currGPU == 'sol_multi':
     # training config
 
     FLAGS['num_epochs'] = 50
-    FLAGS['batch_size'] = 768
-    FLAGS['gradient_accumulation_iterations'] = 1
+    FLAGS['batch_size'] = 192
+    FLAGS['gradient_accumulation_iterations'] = 4
 
     FLAGS['base_learning_rate'] = 3e-3
     FLAGS['base_batch_size'] = 2048
@@ -601,7 +602,7 @@ elif currGPU == 'sol_multi':
     FLAGS['opt_dist'] = False
     FLAGS['splc'] = False
     FLAGS['splc_start_epoch'] = 0
-    FLAGS['norm_weighted_loss'] = True
+    FLAGS['norm_weighted_loss'] = False
 
     FLAGS['finetune'] = False    #actually a linear probe of a frozen model
     FLAGS['compile_model'] = False
