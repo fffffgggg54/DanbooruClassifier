9e8372f510ae2f3910802433bb194dc8cda6f2c7
Fredo Guan <fredo.guan@hotmail.com>
2026-09-20 19:04:56 -0700

    update

--- trainer.py
+++ trainer.py
@@ -415,10 +415,10 @@ elif currGPU == 'v100':
 
     FLAGS['num_epochs'] = 50
     # prefer 128 for davit_tiny
-    FLAGS['batch_size'] = 128
+    FLAGS['batch_size'] = 96
     FLAGS['gradient_accumulation_iterations'] = 1
 
-    FLAGS['base_learning_rate'] = 1e-3
+    FLAGS['base_learning_rate'] = 3e-3
     FLAGS['base_batch_size'] = 2048
     FLAGS['learning_rate'] = ((FLAGS['batch_size'] * FLAGS['gradient_accumulation_iterations']) / FLAGS['base_batch_size']) * FLAGS['base_learning_rate']
     FLAGS['lr_warmup_epochs'] = 5
